ecda7a5860cddbd0cc933209b2c530f85b263928
KDShawn <alimzhan.mamurbekov@mail.ru>
2022-09-13 16:08:57 +0300

    first commit

---
+++ main/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render,redirect
+from .models import Task
+from .forms import TaskForm
+
+def index(request):
+    tasks=Task.objects.all()
+    return render(request,'main/index.html',{'title':'Главная страница тайтла','tasks':tasks})
+
+def about(request):
+    return render(request,'main/about.html')
+
+def create(request):
+    error=''
+    if request.method =='POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error= 'Введеные данные некоректны'
+
+
+    form = TaskForm()
+    context={'form': form}
+    return render(request,'main/create.html',context)
